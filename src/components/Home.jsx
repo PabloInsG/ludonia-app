e1d40d65305ba57481aba0f96a704816b881c0b4
Pablo <pablo.insua.gerpe@gmail.com>
2025-05-19 16:10:02 +0200

    Home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -24,7 +24,7 @@ export const Home = () => {
   return (
     <>
     <div className="flex flex-col h-screen">
-        <div className="lg:fixed -z-10 flex w-full lg:h-[100vh] h-1/3">
+        <div className="lg:fixed -z-10 flex w-full lg:h-[100vh] h-1/6">
             <div className="lg:w-1/3 w-full flex flex-col justify-between lg:bg-[#091F92]">
                 <div className="lg:w-70 w-full lg:h-1/2 h-full flex flex-col lg:justify-center gap-y-2 mt-2">
                     <h1 className='xl:text-8xl text-center text-white opacity-100'>LUDONÍA</h1>
